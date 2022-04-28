9e4e4df18e3ae55490e41656c4a86d6ec5267416
Thierry Paul <devbox@Thierrys-MacBook-Pro.local>
2022-04-28 00:14:08 -0700

    added server example

--- simple-tcp/server/server_test.go
+++ simple-tcp/server/server_test.go
@@ -1,72 +1,42 @@
 package server
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"log"
 	"net"
 	"testing"
+	"time"
 )
 
 // FIXME
 func TestServe(t *testing.T) {
 
-	// Listen on TCP port 2000 on all available unicast and
-	// anycast IP addresses of the local system.
-	l, err := net.Listen("tcp", ":12345")
+	const address = "localhost:12345"
+
+	// Start the server in a goroutine. This allows for a non-blocking call to
+	// `Serve()`.
+	go Serve(address)
+
+	time.Sleep(time.Second)
+
+	conn, err := net.Dial("tcp", address)
+
 	if err != nil {
+		t.Fatalf("Failed to dial: %v", err)
+	}
+
+	if _, err := conn.Write([]byte("Hello, World!")); err != nil {
 		t.Fatal(err)
-	}
-	defer l.Close()
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		log.Printf("Received connection from %s", conn.RemoteAddr())
-
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-
-		// Use a channel to hand off errors
-		errs := make(chan error)
-		go func(c net.Conn) {
-
-			// Make a buffer to hold incoming message
-			buffer := make([]byte, 1024)
-
-			// Read the incoming connection into the buffer.
-			bytes, err := c.Read(buffer) // read the message
-
-			// Check for errors
-			if err != nil {
-				if errors.Is(err, io.EOF) { // prefered way by GoLang doc
-					// Check for EOF
-					fmt.Println("Finished reading from client: Received EOF")
-				} else {
-					// Check for other errors
-
-					// t.Fatal("Error reading: ", err.Error())
-					errs <- err
-				}
-			}
-
-			// Log the received message and size
-			log.Printf("Received %d bytes: %s", bytes, string(buffer[:bytes]))
-
-			// Shut down the connection.
-			c.Close()
-		}(conn)
-
-		// Check for errors
-		err = <-errs
-		if err != nil {
-			t.Fatal("Error reading: ", err.Error())
-		}
+	} else {
+		t.Log("Sent message")
 	}
 
+	// TODO: Check that the server received the correct message.
+
+	conn.Close()
+}
+
+func ExampleServe() {
+	const address = "localhost"
+	const port = "12345"
+
+	Serve(address + ":" + port)
 }
